d5503764fc28a338e9b80c58c53bbf3040b1ab9e
dmitrykrasovskih <dmitry.krasovskih@gmail.com>
2016-03-06 23:58:51 +0200

    - fixed runner scripts

--- src/dist/bin/start.sh
+++ src/dist/bin/start.sh
@@ -4,4 +4,4 @@ set LYKKE_ME_PROTOTYPE_OPTS="-Dlog4j.configuration=file:///$BASEDIR/cfg/log4j.pr
 mkdir "$BASEDIR"/log 2>/dev/null
 mkdir "$BASEDIR"/work 2>/dev/null
 
-sh ./lykke-me-prototype
+sh ./lykke-me-prototype "$BASEDIR"/cfg/application.properties
