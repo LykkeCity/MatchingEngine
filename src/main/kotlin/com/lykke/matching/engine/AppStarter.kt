ec3f2beccc53008e5654335c6945c9c621f45862
dmitrykrasovskih <dmitry.krasovskih@gmail.com>
2016-06-13 12:15:10 +0300

    build details logging

--- src/main/kotlin/com/lykke/matching/engine/AppStarter.kt
+++ src/main/kotlin/com/lykke/matching/engine/AppStarter.kt
@@ -6,7 +6,11 @@ import com.lykke.matching.engine.logging.LoggableObject
 import com.lykke.matching.engine.logging.ME_STATUS
 import com.lykke.matching.engine.logging.MetricsLogger
 import com.lykke.matching.engine.socket.SocketServer
+import com.lykke.quotes.provider.utils.AppVersion
 import org.apache.log4j.Logger
+import java.io.File
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import java.util.concurrent.LinkedBlockingQueue
 
 val LOGGER = Logger.getLogger("AppStarter")
@@ -16,6 +20,16 @@ fun main(args: Array<String>) {
         LOGGER.error("Config file is not provided, stopping application")
         return
     }
+
+    teeLog("Application launched at " + LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss")))
+    teeLog("Revision-number: " + AppVersion.REVISION_NUMBER)
+    teeLog("Build-number: " + AppVersion.BUILD_NUMBER)
+    teeLog("Build-time: " + AppVersion.BUILD_TIME)
+    teeLog("Build-Jdk: " + AppVersion.BUILD_JDK)
+    teeLog("Build-project: " + AppVersion.PROJECT_NAME)
+    teeLog("Working-dir: " + File(".").absolutePath)
+    teeLog("Java-Info: " + System.getProperty("java.vm.name") + " (" + System.getProperty("java.version") + ")")
+
     val config = loadLocalConfig(args[0])
 
     MetricsLogger.init(config.getProperty("metric.logger.key.value"), config.getProperty("metric.logger.line"))
@@ -23,6 +37,11 @@ fun main(args: Array<String>) {
     Runtime.getRuntime().addShutdownHook(ShutdownHook(config.getProperty("metric.logger.key.value")))
 
     SocketServer(config).run()
+}
+
+private fun teeLog(message: String) {
+    println(message)
+    LOGGER.info(message)
 }
 
 internal class ShutdownHook(val link: String) : Thread() {
