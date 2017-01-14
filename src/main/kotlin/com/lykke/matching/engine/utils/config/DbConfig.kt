42f86a7330b2ea574b02801d4f6b515a8b39f11d
dmitrykrasovskih <dmitry.krasovskih@gmail.com>
2017-01-14 23:34:14 +0200

    removed unused connection strings

--- src/main/kotlin/com/lykke/matching/engine/utils/config/DbConfig.kt
+++ src/main/kotlin/com/lykke/matching/engine/utils/config/DbConfig.kt
@@ -8,16 +8,7 @@ data class DbConfig(
     val hMarketOrdersConnString: String,
     val hTradesConnString: String,
     val hLiquidityConnString: String,
-    val backOfficeConnString: String,
-    val logsConnString: String,
     val dictsConnString: String,
     val bitCoinQueueConnectionString: String,
-    val metricsConnectionString: String,
-    val olapLogsConnString: String,
-    val olapConnString: String,
-    val marketMakerConnString: String,
-    val sharedStorageConnString: String,
-    val limitOrdersConnString: String,
-    val bitcoinHandlerConnString: String,
-    val solarCoinConnString: String
+    val sharedStorageConnString: String
 )
