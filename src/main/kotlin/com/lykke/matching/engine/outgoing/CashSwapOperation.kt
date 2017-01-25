0d438634a9e1caa349001d61fd28fdb2b7bc8ff3
dmitrykrasovskih <dmitry.krasovskih@gmail.com>
2017-01-25 13:47:39 +0200

    cash swap operation - fixed build

--- src/main/kotlin/com/lykke/matching/engine/outgoing/CashSwapOperation.kt
+++ src/main/kotlin/com/lykke/matching/engine/outgoing/CashSwapOperation.kt
@@ -2,6 +2,6 @@ package com.lykke.matching.engine.outgoing
 
 import java.util.Date
 
-class CashSwapOperation(val id: String, val externalId: String, val dateTime: Date,
+class CashSwapOperation(val id: String, val dateTime: Date,
                     val clientId1: String, val asset1: String, val volume1: String,
                     val clientId2: String, val asset2: String, val volume2: String): JsonSerializable()
