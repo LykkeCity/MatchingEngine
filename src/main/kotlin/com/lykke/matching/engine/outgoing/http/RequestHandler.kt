5d81c2271a1d592cc510c29f83d8794130dff0f8
dmitrykrasovskih <dmitry.krasovskih@gmail.com>
2017-02-07 18:05:20 +0200

    LWDEV-1473 Add order book snapshot load via http - log requests

--- src/main/kotlin/com/lykke/matching/engine/outgoing/http/RequestHandler.kt
+++ src/main/kotlin/com/lykke/matching/engine/outgoing/http/RequestHandler.kt
@@ -5,23 +5,33 @@ import com.lykke.matching.engine.outgoing.messages.OrderBook
 import com.lykke.matching.engine.services.GenericLimitOrderService
 import com.sun.net.httpserver.HttpExchange
 import com.sun.net.httpserver.HttpHandler
+import org.apache.log4j.Logger
 import java.util.Date
 import java.util.LinkedList
 
 class RequestHandler (val genericLimitOrderService: GenericLimitOrderService) : HttpHandler {
-    override fun handle(exchange: HttpExchange) {
-        val books = LinkedList<OrderBook>()
-        val now = Date()
-        genericLimitOrderService.getAllOrderBooks().values.forEach {
-            val orderBook = it.copy()
-            books.add(OrderBook(orderBook.assetId, true, now, orderBook.bidOrderBook))
-            books.add(OrderBook(orderBook.assetId, false, now, orderBook.askOrderBook))
-        }
+    companion object {
+        val LOGGER = Logger.getLogger(RequestHandler::class.java.name)
+    }
 
-        val response = Gson().toJson(books)
-        exchange.sendResponseHeaders(200, response.length.toLong())
-        val os = exchange.responseBody
-        os.write(response.toByteArray())
-        os.close()
+    override fun handle(exchange: HttpExchange) {
+        try {
+            val books = LinkedList<OrderBook>()
+            val now = Date()
+            genericLimitOrderService.getAllOrderBooks().values.forEach {
+                val orderBook = it.copy()
+                books.add(OrderBook(orderBook.assetId, true, now, orderBook.bidOrderBook))
+                books.add(OrderBook(orderBook.assetId, false, now, orderBook.askOrderBook))
+            }
+
+            val response = Gson().toJson(books)
+            exchange.sendResponseHeaders(200, response.length.toLong())
+            val os = exchange.responseBody
+            os.write(response.toByteArray())
+            os.close()
+            LOGGER.info("Order book snapshot sent to ${exchange.remoteAddress}")
+        } catch (e: Exception) {
+            LOGGER.error("Unable to write order book snapshot request to ${exchange.remoteAddress}", e)
+        }
     }
 }
