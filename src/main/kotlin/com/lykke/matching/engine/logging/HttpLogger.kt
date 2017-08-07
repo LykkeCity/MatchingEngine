720936796044cf398e857e73c66df13fe038ba2f
dmitrykrasovskih <dmitry.krasovskih@gmail.com>
2017-08-08 00:55:28 +0300

    add timeout to metrics logger

--- src/main/kotlin/com/lykke/matching/engine/logging/HttpLogger.kt
+++ src/main/kotlin/com/lykke/matching/engine/logging/HttpLogger.kt
@@ -1,5 +1,6 @@
 package com.lykke.matching.engine.logging
 
+import org.apache.http.client.config.RequestConfig
 import org.apache.http.client.methods.HttpPost
 import org.apache.http.entity.StringEntity
 import org.apache.http.impl.client.HttpClientBuilder
@@ -19,15 +20,19 @@ class HttpLogger(val path: String, val queue: BlockingQueue<LoggableObject>) : T
     }
 
     fun sendHttpRequest(obj: LoggableObject) {
-        try {
-            val httpClient = HttpClientBuilder.create().build()
-            val request = HttpPost(path)
-            val params = StringEntity(obj.getJson())
-            request.addHeader("content-type", "application/json")
-            request.entity = params
-            httpClient.execute(request)
-        } catch (e : Exception) {
-            LOGGER.error("Unable to write log to http: ${e.message}", e)
+        while (true) {
+            try {
+                val requestConfig = RequestConfig.custom().setConnectTimeout(10 * 1000).build()
+                val httpClient = HttpClientBuilder.create().setDefaultRequestConfig(requestConfig).build()
+                val request = HttpPost(path)
+                val params = StringEntity(obj.getJson())
+                request.addHeader("content-type", "application/json")
+                request.entity = params
+                httpClient.execute(request)
+                return
+            } catch (e : Exception) {
+                LOGGER.error("Unable to write log to http: ${e.message}", e)
+            }
         }
     }
 }
