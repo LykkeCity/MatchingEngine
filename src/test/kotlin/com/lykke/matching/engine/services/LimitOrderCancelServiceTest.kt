8426eeb3d9b046da8beed0a1bf37ff64658dd99f
barsukovsky.mo <barsukovsky.mo@gmail.com>
2017-11-30 10:35:54 +0500

    fixed: removing order from client orders map during cancel limit order request

--- src/test/kotlin/com/lykke/matching/engine/services/LimitOrderCancelServiceTest.kt
+++ src/test/kotlin/com/lykke/matching/engine/services/LimitOrderCancelServiceTest.kt
@@ -19,6 +19,7 @@ import com.lykke.matching.engine.outgoing.messages.OrderBook
 import com.lykke.matching.engine.utils.MessageBuilder
 import com.lykke.matching.engine.utils.MessageBuilder.Companion.buildLimitOrder
 import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
 import org.junit.Before
 import org.junit.Test
 import java.util.concurrent.LinkedBlockingQueue
@@ -42,8 +43,8 @@ class LimitOrderCancelServiceTest {
 
     @Before
     fun setUp() {
-        testFileDatabaseAccessor.addLimitOrder(buildLimitOrder(uid = "3", price = 300.0))
         testFileDatabaseAccessor.addLimitOrder(buildLimitOrder(uid = "5", price = -100.0))
+        testFileDatabaseAccessor.addLimitOrder(buildLimitOrder(uid = "3", price = 300.0))
         testFileDatabaseAccessor.addLimitOrder(buildLimitOrder(uid = "6", price = -200.0))
         testFileDatabaseAccessor.addLimitOrder(buildLimitOrder(uid = "7", price = -300.0))
         testFileDatabaseAccessor.addLimitOrder(buildLimitOrder(uid = "8", price = -400.0))
@@ -59,8 +60,9 @@ class LimitOrderCancelServiceTest {
 
     @Test
     fun testCancel() {
-        val service = LimitOrderCancelService(GenericLimitOrderService(testFileDatabaseAccessor, assetsHolder,
-                assetsPairsHolder, balancesHolder, tradesInfoQueue, quotesNotificationQueue), limitOrdersQueue, assetsHolder, assetsPairsHolder, balancesHolder, orderBookQueue, rabbitOrderBookQueue)
+        val genericService = GenericLimitOrderService(testFileDatabaseAccessor, assetsHolder,
+                assetsPairsHolder, balancesHolder, tradesInfoQueue, quotesNotificationQueue)
+        val service = LimitOrderCancelService(genericService, limitOrdersQueue, assetsHolder, assetsPairsHolder, balancesHolder, orderBookQueue, rabbitOrderBookQueue)
         service.processMessage(MessageBuilder.buildLimitOrderCancelWrapper("3"))
 
         assertEquals(1, orderBookQueue.size)
@@ -68,5 +70,8 @@ class LimitOrderCancelServiceTest {
         val order = testFileDatabaseAccessor.loadLimitOrders().find { it.id == "3" }
         assertNull(order)
         assertEquals(4, testFileDatabaseAccessor.loadLimitOrders().size)
+        val previousOrders = genericService.getAllPreviousOrders("Client1", "EURUSD", true)
+        assertEquals(4, previousOrders.size)
+        assertFalse(previousOrders.any { it.externalId == "3" })
     }
 }
