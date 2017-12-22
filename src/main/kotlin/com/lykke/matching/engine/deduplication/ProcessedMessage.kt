a18f012903d36088eb206b112e498600190cb4aa
dmitrykrasovskih <dmitry.krasovskih@gmail.com>
2017-12-23 01:40:23 +0200

    LWDEV-4752 Prevent duplicate messages processing

--- src/main/kotlin/com/lykke/matching/engine/deduplication/ProcessedMessage.kt
+++ src/main/kotlin/com/lykke/matching/engine/deduplication/ProcessedMessage.kt
@@ -1,10 +1,12 @@
 package com.lykke.matching.engine.deduplication
 
+import java.io.Serializable
+
 data class ProcessedMessage(
     val type: Byte,
     val timestamp: Long,
     val messageId: String
-) {
+): Serializable {
     override fun toString(): String {
         return "ProcessedMessage(type=$type, timestamp=$timestamp, messageId='$messageId')"
     }
