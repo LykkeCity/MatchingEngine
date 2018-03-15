0b866a23bfd1746fef75d61b0497aca873ad6807
Oleksandr <rocketmannot@gmail.com>
2018-03-15 21:14:44 +0200

    load setting from database

--- src/main/kotlin/com/lykke/matching/engine/utils/config/ApplicationProperties.kt
+++ src/main/kotlin/com/lykke/matching/engine/utils/config/ApplicationProperties.kt
@@ -1,72 +1,38 @@
 package com.lykke.matching.engine.utils.config
 
-import com.lykke.matching.engine.database.ConfigDatabaseAccessor
+import com.lykke.matching.engine.database.azure.AzureConfigDatabaseAccessor
 import com.lykke.matching.engine.database.cache.DataCache
 import com.lykke.utils.logging.MetricsLogger
 import com.lykke.utils.logging.ThrottlingLogger
-import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.fixedRateTimer
 
-class ApplicationProperties(private val configDatabaseAccessor: ConfigDatabaseAccessor,
+class ApplicationProperties(private val configDatabaseAccessor: AzureConfigDatabaseAccessor,
                             updateInterval: Long? = null) : DataCache() {
     companion object {
         private val LOGGER = ThrottlingLogger.getLogger(ApplicationProperties::class.java.name)
         private val METRICS_LOGGER = MetricsLogger.getLogger()
-        private const val PROP_NAME_REJECT_INVALID_PRICE = "REJECT_INVALID_PRICE"
-    }
 
-    private var properties: Map<String, String> = emptyMap()
-    private val lock = ReentrantLock()
-
-    override fun update() {
-        configDatabaseAccessor.loadConfigs()?.let { properties = it }
-    }
-
-    private fun lockUpdate() {
-        lock.lock()
-        try {
-            update()
-        } finally {
-            lock.unlock()
-        }
-    }
-
-    val rejectInvalidPrice: Boolean
-        get() = getBoolean(PROP_NAME_REJECT_INVALID_PRICE, false)
-
-    private fun getBoolean(name: String, defaultValue: Boolean? = null): Boolean {
-        val value = properties[name]
-        try {
-            if (value == null && defaultValue != null) {
-                return defaultValue
-            }
-            return value!!.toBoolean()
-        } catch (e: Exception) {
-            val message = "Incorrect boolean property value (name=$name, value=$value)"
-            LOGGER.error(message, e)
-            METRICS_LOGGER.logError(message, e)
-            throw e
-        }
-    }
-
-    private fun setBoolean(name: String, value: Boolean) = setValue(name, value.toString())
-
-    private fun setValue(name: String, value: String) {
-        lock.lock()
-        try {
-            configDatabaseAccessor.saveValue(name, value)
-            update()
-        } finally {
-            lock.unlock()
-        }
+        private val DISABLED_ASSETS = "DisabledAssets"
+        private val TRUSTED_CLIENTS = "TrustedClients"
     }
 
     init {
-        lockUpdate()
+        update()
         if (updateInterval != null) {
             fixedRateTimer(name = "Application Properties Updater", initialDelay = updateInterval, period = updateInterval) {
-                lockUpdate()
+                update()
             }
         }
+    }
+
+    private var settings: Settings = Settings()
+
+    override fun update() {
+        configDatabaseAccessor.loadConfigs()?.let { settings = toSettings(it)}
+    }
+
+
+    private fun toSettings(settingsMap: Map<String, Set<String>>): Settings {
+        return Settings(settingsMap[DISABLED_ASSETS],settingsMap[TRUSTED_CLIENTS])
     }
 }
