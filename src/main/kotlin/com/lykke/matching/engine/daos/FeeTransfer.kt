72096709ac77e5d05d2199a721b74318c386abb8
papchenko <rocketmannot@gmail.com>
2018-05-28 12:47:30 +0300

    LWDEV-6992 implement custom equals hashcode FeeTransfer

--- src/main/kotlin/com/lykke/matching/engine/daos/FeeTransfer.kt
+++ src/main/kotlin/com/lykke/matching/engine/daos/FeeTransfer.kt
@@ -1,5 +1,6 @@
 package com.lykke.matching.engine.daos
 
+import com.lykke.matching.engine.utils.RoundingUtils
 import java.math.BigDecimal
 import java.util.Date
 
@@ -11,4 +12,32 @@ data class FeeTransfer(
         val volume: BigDecimal,
         val asset: String,
         val feeCoef: BigDecimal?
-)
+) {
+      override fun equals(other: Any?): Boolean {
+          if (this === other) return true
+          if (javaClass != other?.javaClass) return false
+
+          other as FeeTransfer
+
+          if (externalId != other.externalId) return false
+          if (fromClientId != other.fromClientId) return false
+          if (toClientId != other.toClientId) return false
+          if (dateTime != other.dateTime) return false
+          if (RoundingUtils.equalsIgnoreScale(volume, other.volume)) return false
+          if (asset != other.asset) return false
+          if (RoundingUtils.equalsIgnoreScale(feeCoef, other.feeCoef)) return false
+
+          return true
+      }
+
+      override fun hashCode(): Int {
+          var result = externalId?.hashCode() ?: 0
+          result = 31 * result + fromClientId.hashCode()
+          result = 31 * result + toClientId.hashCode()
+          result = 31 * result + dateTime.hashCode()
+          result = 31 * result + volume.stripTrailingZeros().hashCode()
+          result = 31 * result + asset.hashCode()
+          result = 31 * result + (feeCoef?.stripTrailingZeros()?.hashCode() ?: 0)
+          return result
+      }
+  }
