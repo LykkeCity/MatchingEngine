a6237a4d87cc03aaa725f8a211c0f2db2b8b46d9
papchenko <rocketmannot@gmail.com>
2018-05-30 12:43:12 +0300

    LWDEV-6903 use component stereotype

--- src/main/kotlin/com/lykke/matching/engine/holders/AssetsHolder.kt
+++ src/main/kotlin/com/lykke/matching/engine/holders/AssetsHolder.kt
@@ -3,9 +3,9 @@ package com.lykke.matching.engine.holders
 import com.lykke.matching.engine.daos.Asset
 import com.lykke.matching.engine.database.cache.AssetsCache
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.stereotype.Service
+import org.springframework.stereotype.Component
 
-@Service
+@Component
 class AssetsHolder @Autowired constructor (private val assetsCache: AssetsCache) {
     fun getAsset(assetId: String): Asset {
         val asset = assetsCache.getAsset(assetId)
