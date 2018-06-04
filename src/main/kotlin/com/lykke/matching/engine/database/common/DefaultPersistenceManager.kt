816a306ec0a0596810d74bf7e8290bb29d810e02
papchenko <rocketmannot@gmail.com>
2018-06-04 09:09:44 +0300

    LWDEV-7668 persist message in file

--- src/main/kotlin/com/lykke/matching/engine/database/common/DefaultPersistenceManager.kt
+++ src/main/kotlin/com/lykke/matching/engine/database/common/DefaultPersistenceManager.kt
@@ -2,10 +2,14 @@ package com.lykke.matching.engine.database.common
 
 import com.lykke.matching.engine.database.PersistenceManager
 import com.lykke.matching.engine.database.WalletDatabaseAccessor
+import com.lykke.matching.engine.database.file.FileProcessedMessagesDatabaseAccessor
+import com.lykke.matching.engine.deduplication.ProcessedMessage
 import com.lykke.utils.logging.MetricsLogger
 import org.apache.log4j.Logger
 
-class DefaultPersistenceManager(private val walletDatabaseAccessor: WalletDatabaseAccessor) : PersistenceManager {
+class DefaultPersistenceManager(private val walletDatabaseAccessor: WalletDatabaseAccessor,
+                                private val fileProcessedMessagesDatabaseAccessor: FileProcessedMessagesDatabaseAccessor)
+    : PersistenceManager {
 
     companion object {
         private val LOGGER = Logger.getLogger(DefaultPersistenceManager::class.java.name)
@@ -17,6 +21,7 @@ class DefaultPersistenceManager(private val walletDatabaseAccessor: WalletDataba
     override fun persist(data: PersistenceData): Boolean {
         return try {
             persistData(data)
+            persistProcessedMessages(data.message)
             true
         } catch (e: Exception) {
             val retryMessage = "Unable to save data (${data.details()}), retrying"
@@ -37,6 +42,11 @@ class DefaultPersistenceManager(private val walletDatabaseAccessor: WalletDataba
 
     private fun persistData(data: PersistenceData) {
         walletDatabaseAccessor.insertOrUpdateWallets(data.wallets.toList())
-        return
+    }
+
+    private fun persistProcessedMessages(processedMessage: ProcessedMessage?) {
+        if (processedMessage != null) {
+            fileProcessedMessagesDatabaseAccessor.saveProcessedMessage(processedMessage)
+        }
     }
 }
