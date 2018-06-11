d2856236891f0e8c067f7654a77b818fa85bc186
papchenko <rocketmannot@gmail.com>
2018-06-11 16:21:11 +0300

    LWDEV-6257 fixes

--- src/main/kotlin/com/lykke/matching/engine/keepalive/KeepAliveStarter.kt
+++ src/main/kotlin/com/lykke/matching/engine/keepalive/KeepAliveStarter.kt
@@ -6,16 +6,19 @@ import com.lykke.matching.engine.utils.monitoring.GeneralHealthMonitor
 import com.lykke.utils.AppVersion
 import com.lykke.utils.keepalive.http.KeepAliveStarter
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Profile
 import org.springframework.stereotype.Component
 import javax.annotation.PostConstruct
 
 @Component
 @Profile("default")
-class KeepAliveStarter @Autowired constructor(private val config: Config, private val jedisHolder: JedisHolder) {
+class KeepAliveStarter @Autowired constructor(private val config: Config,
+                                              private val jedisHolder: JedisHolder,
+                                              private val applicationContext: ApplicationContext) {
     @PostConstruct
     private fun start() {
         val generalHealthMonitor = GeneralHealthMonitor(listOf(jedisHolder))
-        KeepAliveStarter.start(config.me.keepAlive, MeIsAliveResponseGetter(generalHealthMonitor), AppVersion.VERSION)
+        KeepAliveStarter.start(config.me.keepAlive, MeIsAliveResponseGetter(generalHealthMonitor, applicationContext), AppVersion.VERSION)
     }
 }
