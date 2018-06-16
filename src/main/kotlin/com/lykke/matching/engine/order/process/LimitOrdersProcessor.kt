e70efe7cde94f78fb624f69f1dc3cd7a15e2f50d
papchenko <rocketmannot@gmail.com>
2018-06-16 11:04:19 +0300

    LWDEV-7834-dev fix merge

--- src/main/kotlin/com/lykke/matching/engine/order/process/LimitOrdersProcessor.kt
+++ src/main/kotlin/com/lykke/matching/engine/order/process/LimitOrdersProcessor.kt
@@ -194,7 +194,7 @@ class LimitOrdersProcessor(assetsHolder: AssetsHolder,
                 OrderStatus.Matched,
                 OrderStatus.InOrderBook,
                 OrderStatus.Processing -> {
-                    if (processMatchingResult(matchingResult, orderCopy, orderInfo, order, limitAsset)) return
+                    if (!processMatchingResult(matchingResult, orderCopy, orderInfo, order, limitAsset)) return
                 }
                 else -> {
                     LOGGER.error("Not handled order status: ${matchingResult.order.status}")
@@ -270,7 +270,7 @@ class LimitOrdersProcessor(assetsHolder: AssetsHolder,
             order.updateStatus(OrderStatus.NotEnoughFunds, matchingResult.timestamp)
             addToReportIfNotTrusted(order)
             processedOrders.add(ProcessedOrder(order, false, message))
-            return true
+            return false
         }
 
         matchingResult.apply()
@@ -331,7 +331,7 @@ class LimitOrdersProcessor(assetsHolder: AssetsHolder,
         buySideOrderBookChanged = true
         sellSideOrderBookChanged = true
         processedOrders.add(ProcessedOrder(order, true))
-        return false
+        return true
     }
 
     private fun validateLimitOrder(order: LimitOrder, orderBook: AssetOrderBook, assetPair: AssetPair, availableBalance: BigDecimal, limitVolume: BigDecimal) {
