380bce5ca062c972b705ede9a0157f9402c6554f
Barsukovskii Mikhail <32326647+barsukovskii@users.noreply.github.com>
2018-08-15 18:54:42 +0500

    LWDEV-8674 Always allow to retry request in case of operation result RUNTIME

--- src/main/kotlin/com/lykke/matching/engine/services/MultiLimitOrderService.kt
+++ src/main/kotlin/com/lykke/matching/engine/services/MultiLimitOrderService.kt
@@ -331,7 +331,8 @@ class MultiLimitOrderService(private val limitOrderService: GenericLimitOrderSer
         }
 
         val updated = walletOperationsProcessor.persistBalances(messageWrapper.processedMessage(), sequenceNumber)
-        messageWrapper.processedMessagePersisted = true
+        messageWrapper.triedToPersist = true
+        messageWrapper.persisted = updated
         if (!updated) {
             LOGGER.error("Unable to save result data (multi limit order id $messageUid)")
             messageWrapper.writeResponse(ProtocolMessages.Response.newBuilder())
@@ -510,7 +511,8 @@ class MultiLimitOrderService(private val limitOrderService: GenericLimitOrderSer
                         messageWrapper.processedMessage(),
                         multiLimitOrder.messageUid, MessageType.MULTI_LIMIT_ORDER,
                         buySideOrderBookChanged, sellSideOrderBookChanged)
-        messageWrapper.processedMessagePersisted = true
+        messageWrapper.triedToPersist = true
+        messageWrapper.persisted = result.success
         val responseBuilder = ProtocolMessages.MultiLimitOrderResponse.newBuilder()
 
         if (!result.success) {
