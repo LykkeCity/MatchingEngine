da72e9b9ec91762f6ee2e4b0ea07f43a566683e4
papchenko <rocketmannot@gmail.com>
2018-08-20 09:32:26 +0300

    LWDEV-8971 add swagger to the project

---
+++ src/main/kotlin/com/lykke/matching/engine/config/spring/SwaggerConfig.kt
@@ -0,0 +1,36 @@
+package com.lykke.matching.engine.config.spring
+
+import com.lykke.utils.AppVersion
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger2.annotations.EnableSwagger2
+import springfox.documentation.service.ApiInfo
+
+@Configuration
+@EnableSwagger2
+open class SwaggerConfig {
+    @Bean
+    open fun apiDocket(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .host("localhost:8080")
+                .apiInfo(ApiInfo(
+                        "Matching Engine",
+                        "Lykke Matching engine",
+                        AppVersion.VERSION,
+                        "",
+                        null,
+                        "",
+                        "",
+                        emptyList()
+                ))
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+
+    }
+}
