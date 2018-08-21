bf6052b35883ff0ec920748dc4b5bd9c4fbbf379
papchenko <rocketmannot@gmail.com>
2018-08-21 09:16:38 +0300

    LWDEV-8998 add order book controller

--- src/main/kotlin/com/lykke/matching/engine/web/controllers/OrderBooksController.kt
+++ src/main/kotlin/com/lykke/matching/engine/web/controllers/OrderBooksController.kt
@@ -1,20 +1,31 @@
 package com.lykke.matching.engine.web.controllers
 
+import com.lykke.matching.engine.outgoing.http.StopOrderBook
 import com.lykke.matching.engine.outgoing.messages.OrderBook
 import com.lykke.matching.engine.services.GenericLimitOrderService
+import com.lykke.matching.engine.services.GenericStopLimitOrderService
+import com.lykke.utils.logging.ThrottlingLogger
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 import java.util.*
+import javax.servlet.http.HttpServletRequest
 
 @RestController
 class OrderBooksController {
 
+    companion object {
+        val LOGGER = ThrottlingLogger.getLogger(OrderBooksController::class.java.name)
+    }
+
     @Autowired
     private lateinit var genericLimitOrderService: GenericLimitOrderService
 
+    @Autowired
+    private lateinit var genericStopLimitOrderService: GenericStopLimitOrderService
+
     @GetMapping("/orderBooks")
-    fun getOrderBooks(): LinkedList<OrderBook> {
+    fun getOrderBooks(request: HttpServletRequest): LinkedList<OrderBook> {
         val books = LinkedList<OrderBook>()
         val now = Date()
 
@@ -24,11 +35,22 @@ class OrderBooksController {
             books.add(OrderBook(orderBook.assetId, false, now, orderBook.getOrderBook(false)))
         }
 
+        LOGGER.info("Order book snapshot sent to ${request.remoteAddr}")
+
         return books
     }
 
     @GetMapping("/stopOrderBooks")
     fun getStopOrderBooks() {
+        val books = LinkedList<StopOrderBook>()
+        val now = Date()
+        genericStopLimitOrderService.getAllOrderBooks().values.forEach {
+            val orderBook = it.copy()
+            books.add(StopOrderBook(orderBook.assetPairId, true, true, now, orderBook.getOrderBook(true, true)))
+            books.add(StopOrderBook(orderBook.assetPairId, true, false, now, orderBook.getOrderBook(true, false)))
+            books.add(StopOrderBook(orderBook.assetPairId, false, true, now, orderBook.getOrderBook(false, true)))
+            books.add(StopOrderBook(orderBook.assetPairId, false, false, now, orderBook.getOrderBook(false, false)))
+        }
 
     }
 }
