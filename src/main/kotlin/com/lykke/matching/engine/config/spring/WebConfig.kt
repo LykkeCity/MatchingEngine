ce42321175bcebdcd83174960912b7eb229c1f02
papchenko <rocketmannot@gmail.com>
2018-08-21 15:20:31 +0300

    LWDEV-8998 use web config to configure ports

---
+++ src/main/kotlin/com/lykke/matching/engine/config/spring/WebConfig.kt
@@ -0,0 +1,35 @@
+package com.lykke.matching.engine.config.spring
+
+import com.lykke.matching.engine.utils.config.Config
+import org.apache.catalina.connector.Connector
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+open class WebConfig  {
+    private companion object {
+        val CONNECTOR_PROTOCOL = "org.apache.coyote.http11.Http11NioProtocol"
+        val SCHEMA = "http"
+    }
+
+    @Autowired
+    private lateinit var config: Config
+
+    @Bean
+    open fun tomcatServletWebServerFactory(): TomcatServletWebServerFactory {
+        val tomcat = TomcatServletWebServerFactory()
+
+        tomcat.addAdditionalTomcatConnectors(getConnector(config.me.httpOrderBookPort))
+
+        return tomcat
+    }
+
+    private fun getConnector(port: Int): Connector {
+        val connector = Connector(CONNECTOR_PROTOCOL)
+        connector.scheme = SCHEMA
+        connector.port = Integer.valueOf(port)
+        return connector
+    }
+}
