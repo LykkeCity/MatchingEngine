a0060f067f2670ea3f17cda1ce5e1dfd8e0f4fcc
papchenko <rocketmannot@gmail.com>
2018-08-21 09:29:59 +0300

    LWDEV-8971 display zero instead of null

--- src/main/kotlin/com/lykke/matching/engine/web/dto/BalanceDto.kt
+++ src/main/kotlin/com/lykke/matching/engine/web/dto/BalanceDto.kt
@@ -2,4 +2,7 @@ package com.lykke.matching.engine.web.dto
 
 import java.math.BigDecimal
 
-class BalanceDto(val balance: BigDecimal? = BigDecimal.ZERO, val reservedBalance: BigDecimal? = BigDecimal.ZERO)
+class BalanceDto(balance: BigDecimal?, reservedBalance: BigDecimal?) {
+    val balance: BigDecimal = balance ?: BigDecimal.ZERO
+    val reservedBalance: BigDecimal = reservedBalance ?: BigDecimal.ZERO
+}
