c714c6da6a09060a0ff21b3e6e26cb7754f555f3
papchenko <rocketmannot@gmail.com>
2018-08-23 14:18:56 +0300

    LWDEV-8998-dev merge fixes

--- src/test/kotlin/com/lykke/matching/engine/AbstractTest.kt
+++ src/test/kotlin/com/lykke/matching/engine/AbstractTest.kt
@@ -31,7 +31,6 @@ import com.lykke.matching.engine.order.cancel.GenericLimitOrdersCancellerFactory
 import com.lykke.matching.engine.order.utils.TestOrderBookWrapper
 import com.lykke.matching.engine.outgoing.messages.CashOperation
 import com.lykke.matching.engine.outgoing.messages.CashTransferOperation
-import com.lykke.matching.engine.outgoing.messages.JsonSerializable
 import com.lykke.matching.engine.outgoing.messages.v2.events.Event
 import com.lykke.matching.engine.outgoing.messages.v2.events.ExecutionEvent
 import com.lykke.matching.engine.outgoing.messages.v2.events.common.BalanceUpdate
