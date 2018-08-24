f85e6cc252332a44308882f60d297b234b202979
papchenko <rocketmannot@gmail.com>
2018-08-24 11:05:31 +0300

    LWDEV-9021 refactoring

--- src/main/kotlin/com/lykke/matching/engine/web/controllers/ClientOrdersController.kt
+++ src/main/kotlin/com/lykke/matching/engine/web/controllers/ClientOrdersController.kt
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-@RestController("/orders")
+@RestController
 @Api(description = "Read only api to access orders from order book")
 class ClientOrdersController {
 
@@ -22,7 +22,7 @@ class ClientOrdersController {
     @Autowired
     private lateinit var genericStopLimitOrderService: GenericStopLimitOrderService
 
-    @GetMapping("client/{clientId}", produces = [MediaType.APPLICATION_JSON_VALUE])
+    @GetMapping("client/{clientId}/orders", produces = [MediaType.APPLICATION_JSON_VALUE])
     @ApiOperation("Endpoint to access orders from current order book for given client")
     fun getOrders(@PathVariable("clientId") clientId: String,
                   @RequestParam(required = false) assetPair: String?,
