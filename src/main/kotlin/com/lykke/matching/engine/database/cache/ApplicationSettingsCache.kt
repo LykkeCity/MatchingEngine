2eee6448dd0e23cf506516423c0d630a1cbba124
papchenko <rocketmannot@gmail.com>
2018-08-28 14:36:37 +0300

    LWDEV-9067 make cache thread safe - update cache

--- src/main/kotlin/com/lykke/matching/engine/database/cache/ApplicationSettingsCache.kt
+++ src/main/kotlin/com/lykke/matching/engine/database/cache/ApplicationSettingsCache.kt
@@ -6,11 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.math.BigDecimal
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+import kotlin.collections.HashSet
 import kotlin.concurrent.fixedRateTimer
 
 @Component
-class ApplicationSettingsCache @Autowired constructor (private val settingsDatabaseAccessor: SettingsDatabaseAccessor,
-                                                       @Value("\${application.settings.update.interval}") updateInterval: Long) : DataCache() {
+class ApplicationSettingsCache @Autowired constructor(private val settingsDatabaseAccessor: SettingsDatabaseAccessor,
+                                                      @Value("\${application.settings.update.interval}") updateInterval: Long) : DataCache() {
 
     private companion object {
         private const val DISABLED_ASSETS = "DisabledAssets"
@@ -19,10 +22,17 @@ class ApplicationSettingsCache @Autowired constructor (private val settingsDatab
         private const val LO_PRICE_DEVIATION_THRESHOLD = "LimitOrderPriceDeviationThreshold"
     }
 
-    private lateinit var trustedClients: Set<String>
-    private lateinit var disabledAssets: Set<String>
-    private lateinit var moPriceDeviationThresholds: Map<String, BigDecimal>,
-    private lateinit var loPriceDeviationThresholds: Map<String, BigDecimal>
+    @Volatile
+    private lateinit var trustedClients: MutableSet<String>
+
+    @Volatile
+    private lateinit var disabledAssets: MutableSet<String>
+
+    @Volatile
+    private lateinit var moPriceDeviationThresholds: MutableMap<String, BigDecimal>
+
+    @Volatile
+    private lateinit var loPriceDeviationThresholds: MutableMap<String, BigDecimal>
 
     init {
         update()
@@ -31,54 +41,79 @@ class ApplicationSettingsCache @Autowired constructor (private val settingsDatab
         }
     }
 
-    @Synchronized
     override fun update() {
         val allSettingGroups = settingsDatabaseAccessor.getAllSettingGroups(true)
+        trustedClients = getSettingValues(allSettingGroups, TRUSTED_CLIENTS)
+        disabledAssets = getSettingValues(allSettingGroups, DISABLED_ASSETS)
 
-        trustedClients = getSettingNameToValueByGroupName(allSettingGroups, TRUSTED_CLIENTS).values.toSet()
-        disabledAssets = getSettingNameToValueByGroupName(allSettingGroups, DISABLED_ASSETS).values.toSet()
-        moPriceDeviationThresholds = getSettingNameToValueByGroupName(allSettingGroups, MO_PRICE_DEVIATION_THRESHOLD).mapValues { BigDecimal(it.value)}
-        loPriceDeviationThresholds = getSettingNameToValueByGroupName(allSettingGroups, LO_PRICE_DEVIATION_THRESHOLD).mapValues { BigDecimal(it.value)}
+        moPriceDeviationThresholds = ConcurrentHashMap(getSettingNameToValueByGroupName(allSettingGroups, MO_PRICE_DEVIATION_THRESHOLD)
+                .mapValues { BigDecimal(it.value) })
+        loPriceDeviationThresholds = ConcurrentHashMap(getSettingNameToValueByGroupName(allSettingGroups, LO_PRICE_DEVIATION_THRESHOLD)
+                .mapValues { BigDecimal(it.value) })
     }
 
-    @Synchronized
     fun isTrustedClient(client: String): Boolean {
         return trustedClients.contains(client)
     }
 
-    @Synchronized
     fun isAssetDisabled(asset: String): Boolean {
         return disabledAssets.contains(asset)
     }
 
-    @Synchronized
     fun marketOrderPriceDeviationThreshold(assetPairId: String): BigDecimal? {
         return moPriceDeviationThresholds[assetPairId]
     }
 
-    @Synchronized
     fun limitOrderPriceDeviationThreshold(assetPairId: String): BigDecimal? {
         return loPriceDeviationThresholds[assetPairId]
     }
 
-    @Synchronized
-    fun addTrustedClient(clientName: String, clientId: String) {
-
+    fun addOrUpdateMarketOrderPriceDeviationThreshold(assetPairId: String, value: BigDecimal) {
+        moPriceDeviationThresholds.put(assetPairId, value)
     }
 
-    @Synchronized
-    fun removeTrustedClient(clientName: String) {
-
+    fun deleteMarketOrderPriceDeviationThreshold(assetPairId: String) {
+        moPriceDeviationThresholds.remove(assetPairId)
     }
 
-    @Synchronized
-    fun addDisabledAsset(assetName: String, assetId: String) {
-
+    fun deleteAllMarketOrderPriceDeviationThresholds() {
+        moPriceDeviationThresholds.clear()
     }
 
-    @Synchronized
-    fun removeDisabledAsset() {
+    fun addOrUpdateLimitOrderPriceDeviationThreshold(assetPairId: String, value: BigDecimal) {
+        loPriceDeviationThresholds.put(assetPairId, value)
+    }
 
+    fun deleteLimitOrdersLimitOrderPriceDeviationThreshold(assetPairId: String) {
+        loPriceDeviationThresholds.remove(assetPairId)
+    }
+
+    fun deleteAllLimitOrdersLimitOrderPriceDeviationThresholds() {
+        loPriceDeviationThresholds.clear()
+    }
+
+    fun addTrustedClient(clientId: String) {
+        trustedClients.add(clientId)
+    }
+
+    fun deleteTrustedClient(clientId: String) {
+        trustedClients.remove(clientId)
+    }
+
+    fun deleteAllTrustedClients() {
+        trustedClients.clear()
+    }
+
+    fun addDisabledAsset(assetId: String) {
+        disabledAssets.add(assetId)
+    }
+
+    fun deleteDisabledAsset(assetId: String) {
+        disabledAssets.remove(assetId)
+    }
+
+    fun deleteAllDisabledAssets() {
+        disabledAssets.clear()
     }
 
     private fun getSettingNameToValueByGroupName(settingGroupName: Set<SettingsGroup>, name: String): Map<String, String> {
@@ -88,5 +123,9 @@ class ApplicationSettingsCache @Autowired constructor (private val settingsDatab
         settings.settings.forEach { result.put(it.name, it.value) }
 
         return result
+    }
+
+    private fun getSettingValues(settingGroupName: Set<SettingsGroup>, name: String): MutableSet<String> {
+        return Collections.synchronizedSet(HashSet(getSettingNameToValueByGroupName(settingGroupName, name).values))
     }
 }
--- src/main/kotlin/com/lykke/matching/engine/services/ApplicationSettingsServiceImpl.kt
+++ src/main/kotlin/com/lykke/matching/engine/services/ApplicationSettingsServiceImpl.kt
@@ -3,6 +3,7 @@ package com.lykke.matching.engine.services
 import com.lykke.matching.engine.daos.setting.Setting
 import com.lykke.matching.engine.daos.setting.SettingsGroup
 import com.lykke.matching.engine.database.azure.AzureSettingsDatabaseAccessor
+import com.lykke.matching.engine.database.cache.ApplicationSettingsCache
 import com.lykke.matching.engine.web.dto.SettingDto
 import com.lykke.matching.engine.web.dto.SettingsGroupDto
 import org.springframework.beans.factory.annotation.Autowired
@@ -13,6 +14,14 @@ class ApplicationSettingsServiceImpl : ApplicationSettingsService {
 
     @Autowired
     private lateinit var azureSettingsDatabaseAccessor: AzureSettingsDatabaseAccessor
+
+    @Autowired
+    private lateinit var applicationSettingsCache: ApplicationSettingsCache
+
+    private val DELETE_SETTING_FROM_CACHE_STRATEGIES = mapOf("" to { settingName: String -> applicationSettingsCache.deleteTrustedClient() },
+            "" to { name: String -> applicationSettingsCache.deleteTrustedClient(name) })
+    private val DELETE_SETTING_GROUP_FROM_CACHE_STRATEGIES = mapOf("" to {settingGroupName: String -> applicationSettingsCache.deleteTrustedClient()})
+
 
     override fun getAllSettingGroups(enabled: Boolean?): Set<SettingsGroupDto> {
         return azureSettingsDatabaseAccessor.getAllSettingGroups(enabled).map { toSettingGroupDto(it) }.toSet()
@@ -40,6 +49,7 @@ class ApplicationSettingsServiceImpl : ApplicationSettingsService {
 
     override fun deleteSetting(settingGroupName: String, settingName: String) {
         azureSettingsDatabaseAccessor.deleteSetting(settingGroupName, settingName)
+        deleteSettingFromCache(settingGroupName, settingName)
     }
 
     private fun toSettingGroupDto(settingGroup: SettingsGroup): SettingsGroupDto {
@@ -53,5 +63,13 @@ class ApplicationSettingsServiceImpl : ApplicationSettingsService {
 
     private fun toSetting(settingDto: SettingDto): Setting {
         return Setting(settingDto.name, settingDto.value, settingDto.enabled, settingDto.comment)
+    }
+
+    private fun deleteSettingFromCache(settingGroupName: String, settingName: String) {
+        DELETE_SETTING_FROM_CACHE_STRATEGIES[settingGroupName]!!.invoke(settingName)
+    }
+
+    private fun deleteSettingGroupFromCache(settingGroupName: String) {
+
     }
 }
