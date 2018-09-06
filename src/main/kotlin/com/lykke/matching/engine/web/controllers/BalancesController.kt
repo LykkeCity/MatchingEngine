6d313fdf7b9e16978e9b14ed0a4b222b4315c317
papchenko <rocketmannot@gmail.com>
2018-09-06 16:06:51 +0300

    LWDEV-9074 return 404 http status on no clinets/assets found

--- src/main/kotlin/com/lykke/matching/engine/web/controllers/BalancesController.kt
+++ src/main/kotlin/com/lykke/matching/engine/web/controllers/BalancesController.kt
@@ -7,7 +7,9 @@ import io.swagger.annotations.Api
 import io.swagger.annotations.ApiOperation
 import org.apache.commons.lang3.StringUtils
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -21,20 +23,26 @@ class BalancesController {
     @GetMapping("/balances", produces = [MediaType.APPLICATION_JSON_VALUE])
     @ApiOperation("Returns balance information for supplied client and assetId")
     fun getBalances(@RequestParam("clientId") clientId: String,
-                    @RequestParam(name = "assetId", required = false, defaultValue = "") assetId: String): List<BalanceDto?>  {
+                    @RequestParam(name = "assetId", required = false, defaultValue = "") assetId: String): ResponseEntity<*> {
 
-        if (StringUtils.isNoneBlank(assetId)) {
-            return listOf(toBalanceDto(assetId, balancesHolder.getBalances(clientId)[assetId]))
+        val balances = balancesHolder.getBalances(clientId)
+
+        if (balances.isEmpty()) {
+            return ResponseEntity("Requested client has no balances", HttpStatus.NOT_FOUND)
         }
 
-        return balancesHolder
-                .getBalances(clientId)
+        if (StringUtils.isNoneBlank(assetId)) {
+            val clientBalance = balances[assetId] ?: return ResponseEntity("No balance found for client, for supplied asset", HttpStatus.NOT_FOUND)
+            return ResponseEntity.ok(listOf(toBalanceDto(assetId, clientBalance)))
+        }
+
+        return ResponseEntity.ok(balances
                 .mapValues { entry -> toBalanceDto(entry.value.asset, entry.value) }
                 .values
-                .toList()
+                .toList())
     }
 
-    private fun toBalanceDto(assetId: String, assetBalance: AssetBalance?): BalanceDto? {
+    private fun toBalanceDto(assetId: String, assetBalance: AssetBalance): BalanceDto? {
         return BalanceDto(assetId, assetBalance?.balance, assetBalance?.reserved)
     }
 }
