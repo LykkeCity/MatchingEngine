a81e94715d8d4b3394e3fd21f45232bdadf9f6c0
Oleksandr Papchenko <rocketmannot@gmail.com>
2018-10-02 23:24:07 +0300

    LWDEV-9570 when ME recovers after maintance mode - write notification to slack

--- src/main/kotlin/com/lykke/matching/engine/utils/monitoring/GeneralHealthMonitor.kt
+++ src/main/kotlin/com/lykke/matching/engine/utils/monitoring/GeneralHealthMonitor.kt
@@ -16,6 +16,8 @@ class GeneralHealthMonitor: HealthMonitor {
         private val METRICS_LOGGER = MetricsLogger.getLogger()
     }
 
+    private var previousMaintenanceModeStatus = false
+
     private val brokenComponents = ConcurrentHashMap.newKeySet<MonitoredComponent>()
 
     override fun ok() = brokenComponents.isEmpty()
@@ -30,11 +32,26 @@ class GeneralHealthMonitor: HealthMonitor {
     }
 
     @Scheduled(fixedRateString = "\${health.check.update.interval}")
-    open fun checkBrokenComponents() {
+    fun checkBrokenComponents() {
         if (!CollectionUtils.isEmpty(brokenComponents)) {
-            val message = "Maintenance mode is on"
-            LOGGER.error(message)
-            METRICS_LOGGER.logError(message)
+            processMaintenanceModeOn()
+        } else if (previousMaintenanceModeStatus) {
+            processMaintenanceModeOff()
         }
+    }
+
+    fun processMaintenanceModeOn() {
+        previousMaintenanceModeStatus = true
+        val message = "Maintenance mode is on, broken component are: $brokenComponents"
+        LOGGER.error(message)
+        METRICS_LOGGER.logError(message)
+    }
+
+
+    fun processMaintenanceModeOff() {
+        previousMaintenanceModeStatus = false
+        val message = "Maintenance mode is off"
+        LOGGER.info(message)
+        METRICS_LOGGER.logWarning(message)
     }
 }
