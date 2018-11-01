ca121b56634fcde527feff4d9c6870a26643c3ef
Barsukovskii Mikhail <barsukovsky.mo@gmail.com>
2018-11-01 10:36:31 +0500

    LWDEV-9861 removed redundant filtering - now it is made in WalletOperationProcessor

--- src/main/kotlin/com/lykke/matching/engine/holders/BalancesHolder.kt
+++ src/main/kotlin/com/lykke/matching/engine/holders/BalancesHolder.kt
@@ -125,7 +125,6 @@ class BalancesHolder(private val balancesDbAccessorsHolder: BalancesDatabaseAcce
     }
 
     fun sendBalanceUpdate(balanceUpdate: BalanceUpdate) {
-        balanceUpdate.balances = balanceUpdate.balances.filter { it.newBalance.compareTo(it.oldBalance) != 0 || it.newReserved.compareTo(it.oldReserved) != 0 }
         if (balanceUpdate.balances.isNotEmpty()) {
             LOGGER.info(balanceUpdate.toString())
             balanceUpdateQueue.put(balanceUpdate)
