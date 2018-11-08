4bc9c1b7701aa86190d0d75c494a835451297159
papchenko <rocketmannot@gmail.com>
2018-11-08 12:16:11 +0200

    LWDEV-9939 refactoring

--- src/main/kotlin/com/lykke/matching/engine/holders/MidPriceHolder.kt
+++ src/main/kotlin/com/lykke/matching/engine/holders/MidPriceHolder.kt
@@ -64,7 +64,7 @@ class MidPriceHolder(@Value("#{Config.me.referenceMidPricePeriod}") private val 
     fun addMidPrice(assetPair: AssetPair, newMidPrice: BigDecimal, operationTime: Date, cancel: Boolean = false) {
         midPriceTimestampByAssetPairId.putIfAbsent(assetPair.assetPairId, operationTime.time)
         removeObsoleteMidPrices(assetPair.assetPairId, getLowerTimeBound(operationTime.time))
-        val midPriceDangerous =  isMidPriceChangeDangerous(assetPair.assetPairId, newMidPrice) && cancel
+        val midPriceDangerous =  isMidPriceChanged(assetPair.assetPairId, newMidPrice) && cancel
 
         val midPrices = midPricesByAssetPairId.getOrPut(assetPair.assetPairId) { LinkedList() }
 
@@ -85,13 +85,13 @@ class MidPriceHolder(@Value("#{Config.me.referenceMidPricePeriod}") private val 
         midPriceRecalculationCount = 0
     }
 
-    fun isMidPriceChangeDangerous(assetPairId: String, newMidPrice: BigDecimal): Boolean {
+    fun isMidPriceChanged(assetPairId: String, newMidPrice: BigDecimal): Boolean {
         val midPrices = midPricesByAssetPairId[assetPairId]
         if (CollectionUtils.isEmpty(midPrices)) {
             return false
         }
 
-        return midPrices!!.last.midPrice > newMidPrice
+        return !NumberUtils.equalsIgnoreScale(midPrices!!.last.midPrice,  newMidPrice)
     }
 
     private fun removeObsoleteMidPrices(assetPairId: String, lowerBoundTimeMillisBound: Long) {
