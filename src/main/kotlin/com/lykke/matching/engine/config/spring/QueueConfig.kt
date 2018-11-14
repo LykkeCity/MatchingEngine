2749b7768699713243b139665fa5a77244473502
Oleksandr Papchenko <rocketmannot@gmail.com>
2018-11-14 14:24:18 +0200

    LWDEV-9805 Monitor Rabbit Mq queue size, when size reaches threshold - turn on maintenance mode

--- src/main/kotlin/com/lykke/matching/engine/config/spring/QueueConfig.kt
+++ src/main/kotlin/com/lykke/matching/engine/config/spring/QueueConfig.kt
@@ -21,16 +21,19 @@ import java.util.concurrent.LinkedBlockingQueue
 open class QueueConfig {
 
     @Bean
+    @RabbitQueue
     open fun clientsEventsQueue(): BlockingQueue<Event<*>> {
         return LinkedBlockingQueue()
     }
 
     @Bean
+    @RabbitQueue
     open fun trustedClientsEventsQueue(): BlockingQueue<ExecutionEvent> {
         return LinkedBlockingQueue()
     }
 
     @Bean
+    @RabbitQueue
     open fun balanceUpdateQueue(): BlockingQueue<BalanceUpdate> {
         return LinkedBlockingQueue<BalanceUpdate>()
     }
@@ -41,11 +44,13 @@ open class QueueConfig {
     }
 
     @Bean
+    @RabbitQueue
     open fun cashSwapQueue(): BlockingQueue<CashSwapOperation> {
         return LinkedBlockingQueue<CashSwapOperation>()
     }
 
     @Bean
+    @RabbitQueue
     open fun clientLimitOrdersQueue(): BlockingQueue<LimitOrdersReport> {
         return LinkedBlockingQueue<LimitOrdersReport>()
     }
@@ -56,38 +61,44 @@ open class QueueConfig {
     }
 
     @Bean
-    open fun orderBookQueue(): BlockingQueue<OrderBook> {
-        return LinkedBlockingQueue<OrderBook>()
-    }
-
-    @Bean
+    @RabbitQueue
     open fun rabbitOrderBookQueue(): BlockingQueue<OrderBook> {
         return LinkedBlockingQueue<OrderBook>()
     }
 
     @Bean
+    @RabbitQueue
     open fun rabbitCashInOutQueue(): BlockingQueue<CashOperation> {
         return LinkedBlockingQueue<CashOperation>()
     }
 
     @Bean
+    @RabbitQueue
     open fun rabbitSwapQueue(): BlockingQueue<MarketOrderWithTrades> {
         return LinkedBlockingQueue<MarketOrderWithTrades>()
     }
 
     @Bean
+    @RabbitQueue
     open fun rabbitTransferQueue(): BlockingQueue<CashTransferOperation> {
         return LinkedBlockingQueue<CashTransferOperation>()
     }
 
     @Bean
+    @RabbitQueue
     open fun reservedCashOperationQueue(): BlockingQueue<ReservedCashOperation> {
         return LinkedBlockingQueue<ReservedCashOperation>()
     }
 
     @Bean
+    @RabbitQueue
     open fun trustedClientsLimitOrdersQueue(): BlockingQueue<LimitOrdersReport> {
         return LinkedBlockingQueue<LimitOrdersReport>()
+    }
+
+    @Bean
+    open fun orderBookQueue(): BlockingQueue<OrderBook> {
+        return LinkedBlockingQueue<OrderBook>()
     }
 
     @Bean
