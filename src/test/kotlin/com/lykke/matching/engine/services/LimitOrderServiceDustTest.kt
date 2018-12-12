6a30b088f792bb46c17029696f20ad1112a0d8dc
papchenko <rocketmannot@gmail.com>
2018-12-12 17:14:20 +0200

    LWDEV-10127-rel merge fixes

--- src/test/kotlin/com/lykke/matching/engine/services/LimitOrderServiceDustTest.kt
+++ src/test/kotlin/com/lykke/matching/engine/services/LimitOrderServiceDustTest.kt
@@ -25,6 +25,7 @@ import org.springframework.test.context.junit4.SpringRunner
 import java.math.BigDecimal
 import kotlin.test.assertEquals
 import com.lykke.matching.engine.utils.assertEquals
+import com.lykke.matching.engine.utils.getSetting
 import org.springframework.beans.factory.annotation.Autowired
 
 
