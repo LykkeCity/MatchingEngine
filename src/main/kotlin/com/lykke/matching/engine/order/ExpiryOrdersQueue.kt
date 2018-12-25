68133e41ff85996ba4761f137ef12d65500a6f35
Barsukovskii Mikhail <barsukovsky.mo@gmail.com>
2018-12-25 15:22:44 +0500

    LWDEV-9019 review fixes

--- src/main/kotlin/com/lykke/matching/engine/order/ExpiryOrdersQueue.kt
+++ src/main/kotlin/com/lykke/matching/engine/order/ExpiryOrdersQueue.kt
@@ -3,42 +3,32 @@ package com.lykke.matching.engine.order
 import com.lykke.matching.engine.daos.LimitOrder
 import org.springframework.stereotype.Component
 import java.util.Date
-import java.util.PriorityQueue
+import java.util.concurrent.ConcurrentHashMap
 
 @Component
 class ExpiryOrdersQueue {
 
-    private val ordersQueue = PriorityQueue<LimitOrder> { o1, o2 ->
-        var result = o1.expiryTime!!.compareTo(o2.expiryTime!!)
-        if (result == 0) {
-            result = o1.createdAt.compareTo(o2.createdAt)
-        }
-        result
-    }
+    private val ordersById = ConcurrentHashMap<String, LimitOrder>()
 
     fun addIfOrderHasExpiryTime(order: LimitOrder): Boolean {
-        if (!order.hasExpiryTime()) {
-            return false
-        }
-        return ordersQueue.add(order)
+        return if (order.hasExpiryTime()) {
+            ordersById[order.id] = order
+            true
+        } else false
     }
 
     fun removeIfOrderHasExpiryTime(order: LimitOrder): Boolean {
-        if (!order.hasExpiryTime()) {
-            return false
-        }
-        return ordersQueue.remove(order)
+        return if (order.hasExpiryTime()) {
+            ordersById.remove(order.id)
+            true
+        } else false
     }
 
     fun getExpiredOrdersExternalIds(date: Date): List<String> {
-        val ordersQueueCopy = PriorityQueue<LimitOrder>(ordersQueue)
-        if (ordersQueueCopy.isEmpty()) {
-            return emptyList()
-        }
-        val result = mutableListOf<String>()
-        while (ordersQueueCopy.peek()?.isExpired(date) == true) {
-            result.add(ordersQueueCopy.poll().externalId)
-        }
-        return result
+        return ordersById.values
+                .asSequence()
+                .filter { it.isExpired(date) }
+                .map { it.externalId }
+                .toList()
     }
 }
