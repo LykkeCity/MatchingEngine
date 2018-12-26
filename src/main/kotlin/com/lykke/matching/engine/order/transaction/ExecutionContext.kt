4c5a33990347f67d4f72da7082be0cb3ff7d65e6
papchenko <rocketmannot@gmail.com>
2018-12-26 14:16:18 +0200

    LWDEV-10257 fix execution context factory

--- src/main/kotlin/com/lykke/matching/engine/order/transaction/ExecutionContext.kt
+++ src/main/kotlin/com/lykke/matching/engine/order/transaction/ExecutionContext.kt
@@ -87,6 +87,7 @@ class ExecutionContext(val messageId: String,
             trustedClientLimitOrdersWithTradesByInternalId.values.forEach {
                 prevExecutionContext.addTrustedClientLimitOrderWithTrades(it)
             }
+            prevExecutionContext.lkkTrades.addAll(this.lkkTrades)
         }
 
         walletOperationsProcessor.apply()
