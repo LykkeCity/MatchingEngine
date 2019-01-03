adae01d9e26f7c04bfa76913ed2ad52991ef74e0
papchenko <rocketmannot@gmail.com>
2019-01-03 11:23:07 +0200

    dev merge fix

--- src/main/kotlin/com/lykke/matching/engine/database/redis/RedisPersistenceManager.kt
+++ src/main/kotlin/com/lykke/matching/engine/database/redis/RedisPersistenceManager.kt
@@ -4,14 +4,11 @@ import com.lykke.matching.engine.common.SimpleApplicationEventPublisher
 import com.lykke.matching.engine.daos.wallet.AssetBalance
 import com.lykke.matching.engine.database.PersistenceManager
 import com.lykke.matching.engine.database.common.entity.MidPricePersistenceData
-import com.lykke.matching.engine.database.common.entity.OrderBooksPersistenceData
 import com.lykke.matching.engine.database.common.entity.PersistenceData
 import com.lykke.matching.engine.database.common.strategy.OrdersPersistInSecondaryDbStrategy
 import com.lykke.matching.engine.database.common.strategy.PersistOrdersStrategy
 import com.lykke.matching.engine.database.reconciliation.events.AccountPersistEvent
 import com.lykke.matching.engine.database.reconciliation.events.MidPricesPersistEvent
-import com.lykke.matching.engine.database.reconciliation.events.OrderBookPersistEvent
-import com.lykke.matching.engine.database.reconciliation.events.StopOrderBookPersistEvent
 import com.lykke.matching.engine.database.redis.accessor.impl.*
 import com.lykke.matching.engine.database.redis.connection.RedisConnection
 import com.lykke.matching.engine.deduplication.ProcessedMessage
@@ -65,65 +62,68 @@ class RedisPersistenceManager(
     }
 
     private fun persistData(transaction: Transaction, data: PersistenceData, startTime: Long) {
-             persistBalances(transaction, data.balancesData?.balances)
-            persistProcessedMessages(transaction, data.processedMessage)
+        persistBalances(transaction, data.balancesData?.balances)
+        persistProcessedMessages(transaction, data.processedMessage)
 
-            if (data.processedMessage?.type == MessageType.CASH_IN_OUT_OPERATION.type ||
-                    data.processedMessage?.type == MessageType.CASH_TRANSFER_OPERATION.type) {
-                persistProcessedCashMessage(transaction, data.processedMessage)
-            }
+        if (data.processedMessage?.type == MessageType.CASH_IN_OUT_OPERATION.type ||
+                data.processedMessage?.type == MessageType.CASH_TRANSFER_OPERATION.type) {
+            persistProcessedCashMessage(transaction, data.processedMessage)
+        }
 
-            val startPersistOrders = System.nanoTime()
-            persistOrders(transaction, data)
-            val endPersistOrders = System.nanoTime()
+        val startPersistOrders = System.nanoTime()
+        persistOrders(transaction, data)
+        val endPersistOrders = System.nanoTime()
 
-            persistMessageSequenceNumber(transaction, data.messageSequenceNumber)
-            persistMidPrices(data.midPricePersistenceData)
+        persistMessageSequenceNumber(transaction, data.messageSequenceNumber)
+        persistMidPrices(data.midPricePersistenceData)
 
-            val persistTime = System.nanoTime()
+        val persistTime = System.nanoTime()
 
-            transaction.exec()
-            val commitTime = System.nanoTime()
-            val nonRedisOrdersPersistTime = if (persistOrdersStrategy.isRedisTransactionUsed()) 0 else endPersistOrders - startPersistOrders
-            val messageId = data.processedMessage?.messageId
-            REDIS_PERFORMANCE_LOGGER.debug("Total: ${PrintUtils.convertToString2((commitTime - startTime - nonRedisOrdersPersistTime).toDouble())}" +
-                    ", persist: ${PrintUtils.convertToString2((persistTime - startTime - nonRedisOrdersPersistTime).toDouble())}" +
-                    (if(nonRedisOrdersPersistTime != 0L) ", non redis orders persist time: ${PrintUtils.convertToString2(nonRedisOrdersPersistTime.toDouble())}" else "") +
-                    ", commit: ${PrintUtils.convertToString2((commitTime - persistTime).toDouble())}" +
-                    ", persisted data summary: ${data.getSummary()}" +
-                    (if (messageId != null) ", messageId: ($messageId)" else ""))
+        transaction.exec()
+        val commitTime = System.nanoTime()
+        val nonRedisOrdersPersistTime = if (persistOrdersStrategy.isRedisTransactionUsed()) 0 else endPersistOrders - startPersistOrders
+        val messageId = data.processedMessage?.messageId
+        REDIS_PERFORMANCE_LOGGER.debug("Total: ${PrintUtils.convertToString2((commitTime - startTime - nonRedisOrdersPersistTime).toDouble())}" +
+                ", persist: ${PrintUtils.convertToString2((persistTime - startTime - nonRedisOrdersPersistTime).toDouble())}" +
+                (if (nonRedisOrdersPersistTime != 0L) ", non redis orders persist time: ${PrintUtils.convertToString2(nonRedisOrdersPersistTime.toDouble())}" else "") +
+                ", commit: ${PrintUtils.convertToString2((commitTime - persistTime).toDouble())}" +
+                ", persisted data summary: ${data.getSummary()}" +
+                (if (messageId != null) ", messageId: ($messageId)" else ""))
 
-            currentTransactionDataHolder.getMessageType()?.let {
-                performanceStatsHolder.addPersistTime(it.type, commitTime - startTime)
-            }
+        currentTransactionDataHolder.getMessageType()?.let {
+            performanceStatsHolder.addPersistTime(it.type, commitTime - startTime)
+        }
 
-            if (!CollectionUtils.isEmpty(data.balancesData?.wallets)) {
-                persistedWalletsApplicationEventPublisher.publishEvent(AccountPersistEvent(data.balancesData!!.wallets))
-            }
+        if (!CollectionUtils.isEmpty(data.balancesData?.wallets)) {
+            persistedWalletsApplicationEventPublisher.publishEvent(AccountPersistEvent(data.balancesData!!.wallets))
+        }
+
+        data.midPricePersistenceData?.let {
+            persistMidPricesApplicationEventPublisher.publishEvent(MidPricesPersistEvent(data.midPricePersistenceData))
+        }
 
         ordersPersistInSecondaryDbStrategy.persistOrders(data.orderBooksData, data.stopOrderBooksData)
     }
-}
 
-private fun persistOrders(transaction: Transaction, data: PersistenceData) {
-    try {
-        persistOrdersStrategy.persist(transaction, data.orderBooksData, data.stopOrderBooksData)
-    } catch (e: JedisException) {
-        throw e
-    } catch (e: Exception) {
-        transaction.discard()
-        throw e
+
+    private fun persistOrders(transaction: Transaction, data: PersistenceData) {
+        try {
+            persistOrdersStrategy.persist(transaction, data.orderBooksData, data.stopOrderBooksData)
+        } catch (e: JedisException) {
+            throw e
+        } catch (e: Exception) {
+            transaction.discard()
+            throw e
+        }
     }
+
+    private fun persistMidPrices(midPricePersistenceData: MidPricePersistenceData?) {
+        if (midPricePersistenceData == null) {
+            RedisPersistenceManager.LOGGER.trace("Mid price is empty - skipping")
+            return
+        }
         persistMidPricesApplicationEventPublisher.publishEvent(MidPricesPersistEvent(midPricePersistenceData))
-}
-
-private fun persistMidPrices(midPricePersistenceData: MidPricePersistenceData?) {
-    if (midPricePersistenceData == null) {
-        RedisPersistenceManager.LOGGER.trace("Mid price is empty - skipping")
-        return
     }
-    persistMidPricesApplicationEventPublisher.publishEvent(MidPricesPersistEvent(midPricePersistenceData))
-}
 
     private fun persistProcessedMessages(transaction: Transaction, processedMessage: ProcessedMessage?) {
         LOGGER.trace("Start to persist processed messages in redis")
