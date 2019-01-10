e1029b54af42728fe7ebb4bddaba7a4805c1f6dc
Barsukovskii Mikhail <barsukovsky.mo@gmail.com>
2019-01-10 14:02:57 +0500

    LWDEV-9085 review changes

--- src/test/kotlin/com/lykke/matching/engine/services/LimitOrderServiceTest.kt
+++ src/test/kotlin/com/lykke/matching/engine/services/LimitOrderServiceTest.kt
@@ -1877,4 +1877,37 @@ class LimitOrderServiceTest : AbstractTest() {
         assertEquals(0, eventOrder.trades?.size)
         assertEquals(OutgoingOrderStatus.CANCELLED, eventOrder.status)
     }
+
+    @Test
+    fun testImmediateOrCancelOrderFullyMatched() {
+        testOrderBookWrapper.addLimitOrder(buildLimitOrder(clientId = "Client2",
+                assetId = "EURUSD",
+                volume = -10.0,
+                price = 1.2))
+        testOrderBookWrapper.addLimitOrder(buildLimitOrder(clientId = "Client2",
+                assetId = "EURUSD",
+                volume = -10.0,
+                price = 1.3))
+
+        singleLimitOrderService.processMessage(messageBuilder.buildLimitOrderWrapper(buildLimitOrder(clientId = "Client1",
+                assetId = "EURUSD",
+                volume = 15.0,
+                price = 1.4,
+                timeInForce = OrderTimeInForce.IOC)))
+
+        assertOrderBookSize("EURUSD", false, 1)
+        assertOrderBookSize("EURUSD", true, 0)
+
+        assertBalance("Client1", "EUR", 1015.0, 0.0)
+        assertBalance("Client1", "USD", 981.5, 0.0)
+
+        assertEquals(1, clientsEventsQueue.size)
+        val event = clientsEventsQueue.poll() as ExecutionEvent
+        assertEquals(3, event.orders.size)
+        assertEquals(4, event.balanceUpdates?.size)
+
+        val eventOrder = event.orders.single { it.walletId == "Client1" }
+        assertEquals(2, eventOrder.trades?.size)
+        assertEquals(OutgoingOrderStatus.MATCHED, eventOrder.status)
+    }
 }
