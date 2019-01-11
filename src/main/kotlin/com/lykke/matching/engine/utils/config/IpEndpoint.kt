607360437455a3e259f9fe648bc17cd1a02e79ea
Barsukovskii Mikhail <32326647+barsukovskii@users.noreply.github.com>
2019-01-11 22:33:13 +0500

    LWDEV-10316 log current active connections count; check and disconnect inactive client connections (#249)

--- src/main/kotlin/com/lykke/matching/engine/utils/config/IpEndpoint.kt
+++ src/main/kotlin/com/lykke/matching/engine/utils/config/IpEndpoint.kt
@@ -2,5 +2,6 @@ package com.lykke.matching.engine.utils.config
 
 data class IpEndpoint(
     val port: Int,
-    val maxConnections: Int
+    val maxConnections: Int,
+    val lifeTimeMinutes: Long?
 )
