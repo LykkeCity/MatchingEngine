bbf90558aa35c7ec59879f350348e24a7ff39ebf
papchenko <rocketmannot@gmail.com>
2019-02-06 11:25:49 +0200

    LWDEV-10401 add dataqueue health checker

--- src/main/kotlin/com/lykke/matching/engine/config/spring/AppConfiguration.kt
+++ src/main/kotlin/com/lykke/matching/engine/config/spring/AppConfiguration.kt
@@ -47,12 +47,12 @@ open class AppConfiguration {
     }
 
     @Bean
-    open fun rabbitQueueSizeChecker(@RabbitQueue namesToInputQueues: Map<String, BlockingQueue<*>>): QueueSizeHealthChecker {
+    open fun dataQueueSizeChecker(@DataQueue namesToInputQueues: Map<String, BlockingQueue<*>>): QueueSizeHealthChecker {
         return QueueSizeHealthChecker(
-                MonitoredComponent.RABBIT_QUEUE,
+                MonitoredComponent.DATA_QUEUE,
                 namesToInputQueues,
-                config.me.queueConfig.rabbitMaxQueueSizeLimit,
-                config.me.queueConfig.rabbitRecoverQueueSizeLimit)
+                config.me.queueConfig.dataMaxQueueSizeLimit,
+                config.me.queueConfig.dataRecoverQueueSizeLimit)
     }
 
     @Bean
