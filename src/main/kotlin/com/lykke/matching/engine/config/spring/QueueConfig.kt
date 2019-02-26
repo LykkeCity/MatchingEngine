02c138bbf676f6329eac1a04e27835fe2c049c2c
papchenko <rocketmannot@gmail.com>
2019-02-26 15:30:33 +0200

    LWDEV-10422-dev fixes

--- src/main/kotlin/com/lykke/matching/engine/config/spring/QueueConfig.kt
+++ src/main/kotlin/com/lykke/matching/engine/config/spring/QueueConfig.kt
@@ -86,7 +86,7 @@ open class QueueConfig {
     @Bean
     @RabbitQueue
     open fun outgoingEventData(): BlockingQueue<OutgoingEventData> {
-        return LinkedBlockingQueue<OutgoingEventData>();
+        return LinkedBlockingQueue<OutgoingEventData>()
     }
     //</editor-fold>
 
