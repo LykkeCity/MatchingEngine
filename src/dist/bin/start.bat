156e741c6fe49bc69e1e70f0f8cc6e0361927ef1
papchenko <rocketmannot@gmail.com>
2019-04-05 12:20:40 +0300

    LWDEV-10416 review fixes

--- src/dist/bin/start.bat
+++ src/dist/bin/start.bat
@@ -3,6 +3,6 @@ set DIRNAME=%~dp0
 if "%DIRNAME%" == "" set DIRNAME=.
 set APP_HOME=%DIRNAME%..
 
-set LYKKE_ME_PROTOTYPE_OPTS="-Dlog4j.configuration=file:///%APP_HOME%/cfg/log4j2.xml"
+set LYKKE_ME_PROTOTYPE_OPTS="-Dlog4j.configurationFile=file:///%APP_HOME%/cfg/log4j2.xml"
 mkdir ..\log
 call lykke-me-prototype.bat %APP_HOME%/cfg/application.properties > ..\log\out.log 2> ..\log\err.log
